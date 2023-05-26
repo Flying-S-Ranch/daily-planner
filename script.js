d8514980ee36cdaf7ae6a719722f924ce16a5a02
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-05-25 21:57:59 -0500

    adds event listener to save text

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // DEPENDENCIES
 var hours = $(".time-block").toArray();
-console.log(hours)
+// console.log(hours)
+var saveButton = $(".saveBtn").toArray();
+// console.log(saveButton);
+var storedText = $("textarea").toArray();
+// console.log(storedText);
+
+
+// var nineTextEl = document.querySelector("nine-text").value
+// console.log(nineTextEl)
 
 // DATA / STATE
 var time = dayjs()
@@ -17,6 +25,63 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+  
+    saveButton[0].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-9", storedText[0].value)
+    })  
+
+    saveButton[1].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-10", storedText[1].value)
+    })
+
+    saveButton[2].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-11", storedText[2].value)
+    })
+
+    saveButton[3].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-12", storedText[3].value)
+    })
+
+    saveButton[4].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-1", storedText[4].value)
+    })
+
+    saveButton[5].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-2", storedText[5].value)
+    })
+
+    saveButton[6].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-3", storedText[6].value)
+    })
+
+    saveButton[7].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-4", storedText[7].value)
+    })
+
+    saveButton[8].addEventListener('click', function(){
+      console.log("saving your work")
+      localStorage.setItem("hour-5", storedText[8].value)
+    })
+  
+  //  var saveNine = $("#hour-9").children("button")
+  //  console.log(saveNine)
+  //  var savedText = document.querySelector("#text-9").value
+  //  console.log(savedText)
+
+  //  $(saveNine).click(function(event){
+  //   console.log("it's working")
+  //   event.preventDefault();
+  //   localStorage.setItem("text", savedText)
+  //  })
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -25,11 +90,9 @@ $(function () {
   // current hour in 24-hour time?
   function compareTime() {
     for (i = 0; i < hours.length; i++) {
-      console.log(hours[i])
-      console.log(plannerTimes[i])
       var hourEl = $(hours[i])
       if (plannerTimes[i] < currentHour) {
-        console.log("the time is later than " + plannerTimes[i] + ":00")
+        // console.log("the time is later than " + plannerTimes[i] + ":00")
         hourEl.addClass('past')
       } else if (plannerTimes[i] > currentHour) {
         hourEl.addClass('future')
@@ -40,6 +103,7 @@ $(function () {
   }
   
   compareTime();
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
